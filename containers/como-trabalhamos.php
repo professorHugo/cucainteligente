f5522960109685128e28d5f6643c3aa9671a88da
Hugo Christian Pereira Gomes <hugo@n2y.com.br>
2024-04-13 15:59:58 -0300

    V1.1.1
    Adicionado novas fotos, alteração em nossa equipe, alteração em como trabalhamos.

--- containers/como-trabalhamos.php
+++ containers/como-trabalhamos.php
@@ -15,8 +15,8 @@
           <div class="media-body pl-3">
             <h3 class="heading">Limpeza de escritório</h3>
             <p>
-              Mesmo o todo-poderoso Apontar não tem controle sobre os textos cegos, é quase não
-              ortográfico.
+              Oferecemos serviços de limpeza de escritório que vão além do básico, 
+              visando não apenas a estética, mas também a saúde e o bem-estar dos seus colaboradores.
             </p>
             <?php if( !isset($_GET['URL']) ){ ?>
               <p><a href="?URL=Services" class="btn-custom">CONSULTE MAIS INFORMAÇÃO</a></p>
@@ -69,8 +69,8 @@
           <div class="media-body pl-3">
             <h3 class="heading">Limpeza de cozinha</h3>
             <p>
-              Mesmo o todo-poderoso Apontar não tem controle sobre os textos cegos, é quase não
-              ortográfico.
+              Nossa equipe especializada em limpeza de cozinhas garante que seu espaço gastronômico seja 
+              mantido em condições impecáveis de higiene e segurança alimentar.
             </p>
             <?php if( !isset($_GET['URL']) ){ ?>
               <p><a href="?URL=Services" class="btn-custom">CONSULTE MAIS INFORMAÇÃO</a></p>
@@ -106,8 +106,8 @@
           <div class="media-body pl-3">
             <h3 class="heading">Limpeza de janelas</h3>
             <p>
-              Mesmo o todo-poderoso Apontar não tem controle sobre os textos cegos, é quase não
-              ortográfico.
+              Nossa equipe especializada em limpeza de janelas oferece um serviço 
+              meticuloso e eficiente para garantir que suas janelas brilhem com clareza e transparência.
             </p>
             <?php if( !isset($_GET['URL']) ){ ?>
               <p><a href="?URL=Services" class="btn-custom">CONSULTE MAIS INFORMAÇÃO</a></p>
@@ -115,6 +115,45 @@
           </div>
         </div>
       </div>
+
+      <div class="col-md-6 col-lg-4 services ftco-animate">
+        <div class="d-block d-flex">
+          <div class="icon d-flex justify-content-center align-items-center">
+            <span class="flaticon-cleaning"></span>
+          </div>
+          <div class="media-body pl-3">
+            <h3 class="heading">Limpeza pós-obra</h3>
+            <p>
+              Nossa equipe especializada em limpeza pós-obra está pronta para transformar seu 
+              espaço de construção em um ambiente limpo, seguro e habitável.
+            </p>
+            <?php if( !isset($_GET['URL']) ){ ?>
+              <p><a href="?URL=Services" class="btn-custom">CONSULTE MAIS INFORMAÇÃO</a></p>
+            <?php } ?>
+          </div>
+        </div>
+      </div>
+
+
+      <div class="col-md-6 col-lg-4 services ftco-animate">
+        <div class="d-block d-flex">
+          <div class="icon d-flex justify-content-center align-items-center">
+            <span class="flaticon-sprayer"></span>
+          </div>
+          <div class="media-body pl-3">
+            <h3 class="heading">Limpeza em condomínio</h3>
+            <p>
+              Nossa equipe especializada em limpeza de condomínios oferece um serviço completo e confiável 
+              para manter os espaços compartilhados do seu condomínio impecáveis.
+            </p>
+            <?php if( !isset($_GET['URL']) ){ ?>
+              <p><a href="?URL=Services" class="btn-custom">CONSULTE MAIS INFORMAÇÃO</a></p>
+            <?php } ?>
+          </div>
+        </div>
+      </div>
+
+
     </div>
   </div>
 </section>
--- containers/nossa-equipe-section.php
+++ containers/nossa-equipe-section.php
@@ -1,7 +1,8 @@
 <section class="ftco-section ftco-no-pt" style="margin-top: 2rem;">
   <div class="container">
     <div class="row">
-      <div class="col-md-12 col-lg-3 pr-md-4 pb-lg-0 pb-4">
+
+      <div class="col-12">
         <div class="heading-section ftco-animate text-center text-lg-left">
           <span class="subheading">Equipe</span>
           <h2>Nossa equipe</h2>
@@ -16,6 +17,8 @@
           <p><a href="#" class="btn btn-secondary">Ver toda equipe</a></p>
         </div>
       </div>
+
+
       <div class="col-md-4 col-lg-3 ftco-animate d-flex">
         <div class="staff">
           <div class="img-wrap d-flex align-items-stretch">
@@ -36,6 +39,8 @@
           </div>
         </div>
       </div>
+
+
       <div class="col-md-4 col-lg-3 ftco-animate d-flex">
         <div class="staff">
           <div class="img-wrap d-flex align-items-stretch">
@@ -56,6 +61,8 @@
           </div>
         </div>
       </div>
+
+
       <div class="col-md-4 col-lg-3 ftco-animate d-flex">
         <div class="staff">
           <div class="img-wrap d-flex align-items-stretch">
@@ -76,6 +83,29 @@
           </div>
         </div>
       </div>
+
+      <div class="col-md-4 col-lg-3 ftco-animate d-flex">
+        <div class="staff">
+          <div class="img-wrap d-flex align-items-stretch">
+            <div class="img align-self-stretch" style="background-image: url(fotos/FOTO32.jpg);">
+            </div>
+          </div>
+          <div class="text pt-3 px-3 pb-4 text-center">
+            <h3>Bárbara</h3>
+            <span class="position mb-2">Limpador(a) de escritório</span>
+            <div class="faded">
+              <ul class="ftco-social text-center">
+                <!-- <li class="ftco-animate"><a href="#" class="d-flex align-items-center justify-content-center"><span class="fa fa-twitter"></span></a></li>
+                <li class="ftco-animate"><a href="#" class="d-flex align-items-center justify-content-center"><span class="fa fa-facebook"></span></a></li>
+                <li class="ftco-animate"><a href="#" class="d-flex align-items-center justify-content-center"><span class="fa fa-google"></span></a></li>
+                <li class="ftco-animate"><a href="#" class="d-flex align-items-center justify-content-center"><span class="fa fa-instagram"></span></a></li> -->
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
     </div>
   </div>
 </section>
